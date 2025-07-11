f3ebf361d494b55cfe62b2affb968710a629e23d
Benjamin Ridges <brid0004@student.monash.edu>
2025-07-12 08:51:57 +1000

    Initial Commit

---
+++ src/main/java/com/bnm/project/BallnearmeApplication.java
@@ -0,0 +1,12 @@
+package com.bnm.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallnearmeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BallnearmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bnm/project/BallnearmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnm.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallnearmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
