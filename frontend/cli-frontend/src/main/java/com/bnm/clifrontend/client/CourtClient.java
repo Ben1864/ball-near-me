a57856fd23f6cf5eac9129f8831a73520d7a390e
Benjamin Ridges <brid0004@student.monash.edu>
2025-07-13 23:01:54 +1000

    Implementation of fetching nearby courts

--- frontend/cli-frontend/src/main/java/com/bnm/clifrontend/client/CourtClient.java
+++ frontend/cli-frontend/src/main/java/com/bnm/clifrontend/client/CourtClient.java
@@ -4,6 +4,7 @@ package com.bnm.clifrontend.client;
 import com.bnm.clifrontend.model.CourtEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
@@ -20,5 +21,9 @@ public interface CourtClient {
 
     @PostExchange("/api/courts")
     CourtEntity addCourt(@RequestBody CourtEntity court);
+
+    @GetExchange("/api/courts/nearby")
+    List<CourtEntity> getNearbyCourts(@RequestParam double lat, @RequestParam double lng,
+                                      @RequestParam(defaultValue = "10") int limit);
 
 }
--- frontend/cli-frontend/src/main/java/com/bnm/clifrontend/commands/CourtCommands.java
+++ frontend/cli-frontend/src/main/java/com/bnm/clifrontend/commands/CourtCommands.java
@@ -2,6 +2,7 @@ package com.bnm.clifrontend.commands;
 
 import com.bnm.clifrontend.client.CourtClient;
 import com.bnm.clifrontend.client.GeocodingClient;
+import com.bnm.clifrontend.client.UserClient;
 import com.bnm.clifrontend.model.CourtEntity;
 import com.bnm.clifrontend.model.GoogleGeocodingResponse;
 import org.jline.reader.LineReader;
@@ -9,6 +10,7 @@ import org.jline.reader.LineReaderBuilder;
 import org.jline.terminal.Terminal;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -20,13 +22,17 @@ public class CourtCommands {
     private final Terminal terminal;
     private final GeocodingClient geocodingClient;
     private final String apiKey;
+    private final AuthState authState;
+    private final UserClient userClient;
 
 
-    public CourtCommands(CourtClient courtClient, Terminal terminal, GeocodingClient geocodingClient, String apiKey) {
+    public CourtCommands(CourtClient courtClient, Terminal terminal, GeocodingClient geocodingClient, String apiKey, AuthState authState, UserClient userClient) {
         this.courtClient = courtClient;
         this.terminal = terminal;
         this.geocodingClient = geocodingClient;
         this.apiKey = apiKey;
+        this.authState = authState;
+        this.userClient = userClient;
     }
 
     @ShellMethod(key = "show-all-courts", value = "Show all courts")
@@ -59,6 +65,24 @@ public class CourtCommands {
     public String  getCourtById(@PathVariable("id") String id) {
         CourtEntity court = courtClient.getCourtById(id);
         return court.toString();
+    }
+
+    @ShellMethod(key = "get-nearby-courts", value = "Get n closest courts to your location")
+    public String  getNearbyCourts(@ShellOption(help = "limit on closest court responses", defaultValue = "10" )int n) {
+        LineReader reader = LineReaderBuilder.builder().terminal(terminal).build();
+        String location = reader.readLine("Enter your current location: ");
+
+        GoogleGeocodingResponse response = geocodingClient.geocode(location, apiKey);
+        double latitude = response.results().get(0).geometry().location().lat();
+        double longitude = response.results().get(0).geometry().location().lng();
+
+        List<CourtEntity> closestCourts = courtClient.getNearbyCourts(latitude, longitude, n);
+        StringBuilder sb = new StringBuilder();
+        for (CourtEntity court : closestCourts) {
+            sb.append(court.toString());
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
 }
