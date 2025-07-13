2a96f908b85814b9a2c34112657a4f314b0c7cee
Benjamin Ridges <brid0004@student.monash.edu>
2025-07-13 12:15:54 +1000

    init Spring Shell CLI frontend

---
+++ frontend/cli-frontend/src/main/java/com/bnm/clifrontend/CliFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.bnm.clifrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CliFrontendApplication.class, args);
+    }
+
+}
---
+++ frontend/cli-frontend/src/test/java/com/bnm/clifrontend/CliFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnm.clifrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
